19b4fb711da318ef33acf446d57da5ea0c6f0d6f
kigi <kigi.chang@gmail.com>
2015-08-06 18:31:01 +0800

    session 2

--- swift-2.0-sample/main.swift
+++ swift-2.0-sample/main.swift
@@ -8,36 +8,117 @@
 
 import Foundation
 
-// MARK: Session 0
-print("Hello, World!")
+// MARK: Class
+/*
+// Sample
+class Shape {
+    var numberOfSides = 0
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides"
+    }
+}
 
-// MARK: Session 1
-
-
-// MARK: Session 2
-
-
-// MARK: Session 3
-
-
-// MARK: Session 4
-
-
-// MARK: Session 5
-
-
-// MARK: Session 6
+let shape = Shape()
+shape.numberOfSides = 7
+print(shape.simpleDescription())
+*/
 
 
-// MARK: Session 7
+class NamedShape {
+    
+    var numberOfSides: Int = 0
+    
+    let name: String        // 未給值的常數變數
+    
+    init(name: String) {
+        self.name = name    // 在 init 內，對常數指定值
+    }
+    
+    func simpleDescription() -> String {
+        return "A \(name) with \(numberOfSides) sides"
+    }
+    
+    deinit {
+        print("\(name) release")
+    }
+    
+}
+
+/*
+// init and deinit test
+func test() {
+    let s1 = NamedShape(name: "Test1")  // 要帶 "name"，要不然會報錯
+    let s2 = NamedShape(name: "Test2")
+
+    s1.numberOfSides = 10
+    s2.numberOfSides = 20
+
+    print(s1.simpleDescription())
+    print(s2.simpleDescription())
+}
+test()
+*/
+
+/*
+// inheritance and polymorphism
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        
+        self.numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A \(name) with sides of length \(sideLength)."
+    }
+    
+}
 
 
-// MARK: Session 8
+let testSqaure = Square(sideLength: 5.2, name: "my test square")
+print(testSqaure.area())
+print(testSqaure.simpleDescription())
+
+*/
 
 
-// MARK: Session 9
+// getter and setter
+class EquilateralTriangle: NamedShape {
+    
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return Double(numberOfSides) * sideLength
+        }
+        set {
+            print("Got \(newValue)")
+            sideLength = newValue / Double(numberOfSides)
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "A \(name) with side of length \(sideLength) and perimeter \(perimeter)"
+    }
+}
 
+let triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
 
-// MARK: Session 10
-
-
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.perimeter)
